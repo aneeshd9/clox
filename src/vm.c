5c9d56d5fdd030850ef35182761b0ee55cd8f90d
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-02-20 15:13:31 -0500

    Started implementing VM.

---
+++ src/vm.c
@@ -0,0 +1,14 @@
+#include "common.h"
+#include "vm.h"
+
+VM vm;
+
+void initVM() {}
+
+void freeVM() {}
+
+InterpretResult interpret(Chunk *chunk) {
+  vm.chunk = chunk;
+  vm.ip = chunk->code;
+  return run();
+}
