3c1f13ddef8cce324409a164798efbfac0e4abda
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-03-10 10:44:24 -0500

    Finished implementing scanner.

--- src/vm.c
+++ src/vm.c
@@ -1,5 +1,6 @@
 #include "chunk.h"
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -92,8 +93,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
 }
